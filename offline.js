c119dd2b81fddf47330017757de759161c387192
Amir <amireisakhorshidi.fanavary2018@gmail.com>
2019-12-19 19:59:53 +0330

    upload app

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1576772766,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/aghab_jolo-sheet0.png",
+		"images/aghab_jolo-sheet1.png",
+		"images/stop_azad-sheet0.png",
+		"images/stop_azad-sheet1.png",
+		"images/photoviewer-sheet0.png",
+		"images/back-sheet0.png",
+		"images/ah_icon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
